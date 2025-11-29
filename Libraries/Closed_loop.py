70a404ff2ab34624ae8df77a21bcd5e03e22ffdb
NyiNyi-14 <naung1@lsu.edu>
2025-11-28 21:54:28 -0600

    Publish 4.1

---
+++ Libraries/Closed_loop.py
@@ -0,0 +1,50 @@
+# %% Import libraries
+import numpy as np
+from scipy.integrate import solve_ivp
+
+# %% Simulation NEW
+class Closed_loop:
+    def __init__(self, system, controller):
+        self.system = system
+        self.controller = controller
+
+    def simulate(self, time, dt, ref, init, control_index=2):
+
+        state = np.array(init, dtype=float)
+        state_dim = len(init)
+        states = []
+
+        if np.isscalar(ref):
+            ref = np.full_like(time, ref)
+
+        for i, t in enumerate(time):
+            mea = state[control_index]
+            u = self.controller.control(mea, ref[i], dt)
+
+            sol = solve_ivp(self.system.ODE, [t, t + dt], state, args=(u,))
+            state = sol.y[:, -1]
+            states.append(state)
+        
+        self.controller.reset() 
+
+        return time, np.array(states)
+
+    def simulate_extra(self, time, dt, ref, init, control_index=2, extra_args_func=None):
+
+        state = np.array(init, dtype=float)
+        states = []
+
+        if np.isscalar(ref):
+            ref = np.full_like(time, ref)
+
+        for i, t in enumerate(time):
+            mea = state[control_index]
+            u = self.controller.control(mea, ref[i], dt)
+
+            extra_args = extra_args_func[i]
+            sol = solve_ivp(self.system.ODE, [t, t + dt], state, args=(u, extra_args))
+            state = sol.y[:, -1]
+            states.append(state)
+
+        self.controller.reset()
+        return time, np.array(states)
